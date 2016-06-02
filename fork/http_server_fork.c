a23d7a7ec8097dbd7d8eedf384a522443bd76d0f
Munkhdorj Baatarsuren <munkhdorj.bs@gmail.com>
2016-06-02 14:53:59 +0900

    ipv4, ipv6 concurrent binding succeeded

--- fork/http_server_fork.c
+++ fork/http_server_fork.c
@@ -15,9 +15,11 @@ int main(int argc, char **argv)
 {
 	struct addrinfo hints, *res0, *res;
 	time_t ticks;
-	int n, f, connfd, sock[20];
-	char *yes = "ens160";
+	int n, f, connfd, sock[100];
+	//char *yes = "ens160";
+	int no = 0;
 	int ok = 1;
+	pid_t pid;
 	char sendBuff[256];
 	char buf[INET_ADDRSTRLEN];
 	char buf1[INET6_ADDRSTRLEN];
@@ -27,7 +29,7 @@ int main(int argc, char **argv)
 	hints.ai_flags = AI_PASSIVE;
 	hints.ai_socktype = SOCK_STREAM;
 
-	if( getaddrinfo("localhost", "31601", &hints, &res0) != 0){
+	if( getaddrinfo("login2.sekiya-lab.info", "31601", &hints, &res0) != 0){
 		herror("getaddrinfo");
 		exit(EXIT_FAILURE);
 	}
@@ -41,48 +43,51 @@ int main(int argc, char **argv)
 			perror("socket");
 			exit(EXIT_FAILURE);
 		}
-		if (setsockopt(sock[n], IPPROTO_IPV6, IPV6_V6ONLY, &yes, sizeof(yes)) < 0) {
-			perror("setsockopt");
+
+		if (setsockopt(sock[n], SOL_SOCKET, SO_REUSEADDR, (const char*)&ok, sizeof(ok)) < 0) {
+			perror("setsockopt SO_REUSEADDR");
 			exit(EXIT_FAILURE);
-		}
-		if ( res->ai_family == AF_INET) {
-			printf("here1\n");
-			printf("ipv4 address : %s\n", inet_ntop(res->ai_family, &((struct sockaddr_in *)(res->ai_addr))->sin_addr, buf, INET_ADDRSTRLEN));
-		} else if ( res->ai_family == AF_INET6) {
-			printf("here2\n");
-			printf("ipv6 address : %s\n", inet_ntop(res->ai_family, &((struct sockaddr_in6 *)(res->ai_addr))->sin6_addr, buf1, INET6_ADDRSTRLEN));
 		}
 
-		/*
-		if (setsockopt(sock[n], IPPROTO_IPV6, IPV6_V6ONLY, &yes, sizeof(yes)) < 0) {
-			perror("setsockopt");
-			exit(EXIT_FAILURE);
+		if ( res->ai_family == AF_INET) {
+			printf("ipv4 address : %s\n", inet_ntop(res->ai_family, &((struct sockaddr_in *)(res->ai_addr))->sin_addr, buf, INET_ADDRSTRLEN));
+		} else if ( res->ai_family == AF_INET6) {
+			if (setsockopt(sock[n], IPPROTO_IPV6, IPV6_V6ONLY, (const char*)&no, sizeof(no)) < 0) {
+				perror("setsockopt IPV6_V6ONLY");
+				exit(EXIT_FAILURE);
+			}
+			printf("ipv6 address : %s\n", inet_ntop(res->ai_family, &((struct sockaddr_in6 *)(res->ai_addr))->sin6_addr, buf1, INET6_ADDRSTRLEN));
 		}
-		*/
-		if (setsockopt(sock[n], SOL_SOCKET, SO_REUSEADDR, (const char*)&ok, sizeof(ok)) < 0) {
-			perror("setsockopt");
-			exit(EXIT_FAILURE);
-		}
+		
 		if (bind(sock[n], res->ai_addr, res->ai_addrlen) < 0) {
 			perror("bind");
 			exit(EXIT_FAILURE);
 		}
-		if (listen(sock[n], LISTENQ) < 0) {
-			perror("listen");
-			exit(EXIT_FAILURE);
-		}
-		while(1) {
-  		connfd = accept(sock[n], (struct sockaddr*)NULL, NULL); 
-      
-			ticks = time(NULL);
-      snprintf(sendBuff, sizeof(sendBuff), "%.24s\r\n", ctime(&ticks));
-      write(connfd, sendBuff, strlen(sendBuff)); 
 
-      close(connfd);
-      sleep(1);
+		if ( (pid = fork() ) == 0 ) {
+			if (listen(sock[n], LISTENQ) < 0) {
+				perror("listen");
+				exit(EXIT_FAILURE);
+			}
+			while(1) {
+				connfd = accept(sock[n], (struct sockaddr*)NULL, NULL); 
+				
+				ticks = time(NULL);
+				snprintf(sendBuff, sizeof(sendBuff), "%.24s\r\n", ctime(&ticks));
+				write(connfd, sendBuff, strlen(sendBuff)); 
+
+				close(connfd);
+				sleep(1);
+			}
 		}
+			
+		printf("here1\n");
 		res = res->ai_next;
+		printf("here2\n");
 		n++;
+		printf("here3\n");
+		/*
+		*/
 	}
 
 	freeaddrinfo(res0);
